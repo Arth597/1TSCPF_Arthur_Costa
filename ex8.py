f5c755922fc931de3ab0e2f2bf94e5646e174d96
Arth597 <168542088+Arth597@users.noreply.github.com>
2024-04-30 08:33:06 -0300

    Add file ex.7 via upload

---
+++ ex8.py
@@ -0,0 +1,10 @@
+# Menor, intermediario, maior:. Entre com 3 números em 3 variáveis diferentes ordene imprimindo também a
+# classificação: menor, intermediário, maior
+
+x = int(input("Digite um numero: "))
+y = int(input("Digite um numero: "))
+z = int(input("Digite um numero: "))
+
+lista = [x,y,z]
+lista.sort()
+print(lista)
